02718e18d106881745d26b9202222d51dd6b6e87
akhaware <akhaware@XO-QA-FC-053.Corp.Elmae>
2017-12-28 12:37:17 +0530

    Reviewed and cahnged code

--- myGitProject1/src/myGitProject1/myTestClass.java
+++ myGitProject1/src/myGitProject1/myTestClass.java
@@ -5,7 +5,8 @@ public class myTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my test git project");
-		System.out.println("Crated test branch1");
+		System.out.println("Crated test branch1 Changed");
+		System.out.println("Crated test branch1 Changed");
 	}
 
 }
