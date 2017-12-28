46fbff7af1715f222adc5b5b4aaefcbf51fd51ab
pborle <pborle@XO-QA-FC-011.Corp.Elmae>
2017-12-28 12:11:53 +0530

    created test branch 1

--- myGitProject1/src/myGitProject1/myTestClass.java
+++ myGitProject1/src/myGitProject1/myTestClass.java
@@ -5,6 +5,7 @@ public class myTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my test git project");
+		System.out.println("Crated test branch1");
 	}
 
 }
