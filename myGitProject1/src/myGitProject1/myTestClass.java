bfaed18da534a93a3d2e21c456f0bae0fa29154c
Prasad <Prasad@Prasad-PC>
2018-01-24 22:01:50 +0530

    changed the code

--- myGitProject1/src/myGitProject1/myTestClass.java
+++ myGitProject1/src/myGitProject1/myTestClass.java
@@ -6,7 +6,7 @@ public class myTestClass {
 		// TODO Auto-generated method stub
 		System.out.println("This is my test git project");
 		System.out.println("Crated test branch1 Changed");
-		System.out.println("Crated test branch1 Changed");
+		System.out.println("Crated test branch1 Changed");;
 	}
 
 }
